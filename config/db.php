82727ccbd496a007102cf565252ff66db736fe5b
Wilterson Garcia <wilterson_donda@hotmail.com>
2017-07-02 09:20:20 -0300

    First Commit

---
+++ config/db.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Wilterson Garcia
+ * Date: 02/07/2017
+ * Time: 08:29
+ */
+
+return [
+    'default_connection' => [
+        'driver' => 'mysql',
+        'host' => 'localhost',
+        'database' => 'genfinance',
+        'username' => 'root',
+        'password' => 'root',
+        'charset' => 'utf8',
+        'collation' => 'utf8_unicode_ci'
+    ]
+];
